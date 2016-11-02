95723b000bb9d230be67cb93a681bb4a55758357
BruceWeng <weng8916@gmail.com>
2016-11-02 16:44:50 -0700

    Create ChatController

---
+++ static/js/controller.js
@@ -0,0 +1,13 @@
+let SpaceChatApp = angular.module('SpaceChatApp', []);
+
+SpaceChatApp.controller('ChatController', function($scope) {
+  let socket = io.connect('http://' + document.domain + ':' + location.port + '/iss');
+
+  $scope.messages = [];
+  $scope.name = '';
+  $scope.text = '';
+
+  socket.on('connect', function() {
+    console.log('Socket Connected');
+  });
+});
