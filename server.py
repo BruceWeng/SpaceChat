79fe9541b98c684f9f03a564a2f889498c716b8b
BruceWeng <weng8916@gmail.com>
2016-11-01 22:34:08 -0700

    Install Flask

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/')
 def mainIndex():
-    print 'in hello world'
+    print ('Enter Index Page...')
     return app.send_static_file('index.html')
-    
+
 
 
 # start the server
 if __name__ == '__main__':
+        print ('Running Server On Port 8080...')
         app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
